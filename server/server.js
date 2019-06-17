14d318fe1aecd7f2ce4d6072a4ba5da8bde08d04
LuckyCode7 <nowak9.private@gmail.com>
2019-06-17 17:42:18 +0200

    Running server via npm command 'start'

--- server/server.js
+++ server/server.js
@@ -5,22 +5,22 @@ let server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            let html = fs.readFileSync('../public/html/index.html', 'utf8');
+            let html = fs.readFileSync('public/html/index.html', 'utf8');
             res.end(html);
             break;
         case '/style/style.css':
             res.writeHead(200, { 'Content-type': 'text/css' });
-            let css = fs.readFileSync('../public/style/style.css', 'utf8');
+            let css = fs.readFileSync('public/style/style.css', 'utf8');
             res.end(css);
             break;
         case '/img/background.png':
             res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-            let jpg = fs.readFileSync('../public/img/background.png', 'binary');
+            let jpg = fs.readFileSync('public/img/background.png', 'binary');
             res.end(jpg, 'binary');
             break;
         case '/dist/bundle.js':
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            let js = fs.readFileSync('../dist/bundle.js', 'utf8');
+            let js = fs.readFileSync('dist/bundle.js', 'utf8');
             res.end(js);
             break;
         default:
